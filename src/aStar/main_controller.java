d6996f6ab43549deb746fff07f6d6c3dc78ab98b
tgibbons-css <tgibbons@css.edu>
2017-01-06 10:31:10 -0600

    Added input for start and goal. Included map in project.

--- src/aStar/main_controller.java
+++ src/aStar/main_controller.java
@@ -1,5 +1,7 @@
 package aStar;
 
+import java.util.Scanner;
+
 public class main_controller {
 	
     private static int start = 1;
@@ -7,8 +9,14 @@ public class main_controller {
     private static Path path;
 
     public static void main(String[] args) { 
+        Scanner scnr = new Scanner(System.in);    // needed to read input from the keyboard
     	System.out.println("MainControl starting up");
-    	
+        
+        System.out.println("What node should pathfinding start with (1-30)");
+        start = scnr.nextInt();                // reads in start node
+        System.out.println("What node should pathfinding end at for a goal (1-30)");
+    	goal = scnr.nextInt();                // reads in end node
+        
     	System.out.println("Map initializing...");
         AreaMap map = new AreaMap();
         
@@ -18,7 +26,7 @@ public class main_controller {
         System.out.println("Calculating shortest path from node "+start+" to node "+goal);
         path = pathFinder.calcShortestPath(start, goal);
                 
-        System.out.println("Starting with Errors --- Printing map of shortest path...");
+        System.out.println("Printing shortest path from start to goal");
         pathFinder.printPath();
     	
     }
